893de0d1209fac591cfd9e4615b14771b42d910b
xinwenbo <xinwenbo@dongao.com>
2025-08-07 19:21:03 +0800

    v4555

--- server-k/build/intermediates/cxx/Debug/6n2f4x5s/logs/arm64-v8a/build_command_ashmem.bat
+++ server-k/build/intermediates/cxx/Debug/6n2f4x5s/logs/arm64-v8a/build_command_ashmem.bat
@@ -1,5 +1,5 @@
 @echo off
 "E:\\Android\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "E:\\Android\\KServer\\server-k\\.cxx\\Debug\\2t5z5f3k\\arm64-v8a" ^
+  "E:\\Android\\KServer\\server-k\\.cxx\\Debug\\6n2f4x5s\\arm64-v8a" ^
   ashmem
